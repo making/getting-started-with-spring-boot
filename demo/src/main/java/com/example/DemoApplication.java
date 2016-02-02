e56f60b166c89690aa7512ae315133355717ff9a
making <makingx@gmail.com>
2016-02-02 18:59:41 +0900

    Define explicitly

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
+	@Bean
+	GreetingService greetingService() {
+		return new PropertyGreetingService();
+	}
+	
 	@Autowired
 	GreetingService greetingService;
 
--- demo/src/main/java/com/example/PropertyGreetingService.java
+++ demo/src/main/java/com/example/PropertyGreetingService.java
@@ -1,9 +1,7 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class PropertyGreetingService implements GreetingService {
 	@Value("${greeting.message}")
 	String message;
