d894e1e51bd182b4d401e12a0a89fb69ce6c6514
making <makingx@gmail.com>
2016-02-02 18:50:28 +0900

    Use @Qualifier

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication implements CommandLineRunner {
 
 	@Autowired
+	@Qualifier("propertyGreetingService")
 	GreetingService greetingService;
 
 	public static void main(String[] args) {
