d8a13d92bb2995d01947efbe8665d8d9868a27eb
making <makingx@gmail.com>
2016-02-02 18:19:37 +0900

    Apply @Component

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,19 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
-	@Bean
-	GreetingService greetingService() {
-		return new SimpleGreetingService();
-	}
-	
 	@Autowired
 	GreetingService greetingService;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
