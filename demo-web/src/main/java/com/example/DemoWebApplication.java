135e495072adda994f6ddbd6b686c6daade82cb7
making <makingx@gmail.com>
2016-02-03 10:45:54 +0900

    Add first controller

--- demo-web/src/main/java/com/example/DemoWebApplication.java
+++ demo-web/src/main/java/com/example/DemoWebApplication.java
@@ -2,10 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoWebApplication {
 
+	@RequestMapping("/")
+	String hello() {
+		return "Hello World!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWebApplication.class, args);
 	}
