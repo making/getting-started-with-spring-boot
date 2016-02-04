50affecd1df1f54fe22f649a45e0068b89e9b672
making <makingx@gmail.com>
2016-02-04 15:27:34 +0900

    Add Spring Data REST

--- demo-web/src/main/java/com/example/CityRepository.java
+++ demo-web/src/main/java/com/example/CityRepository.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CityRepository extends JpaRepository<City, Integer> {
 
 }
