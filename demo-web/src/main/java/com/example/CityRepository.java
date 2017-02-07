927f4632b4e401e37b78d3b377e61e9ed5cb3f1f
making <makingx@gmail.com>
2017-02-07 11:41:26 +0900

    Polish
    (cherry picked from commit 272e1322fa8309fd165fd2b9e740dd911982abbc)

--- demo-web/src/main/java/com/example/CityRepository.java
+++ demo-web/src/main/java/com/example/CityRepository.java
@@ -1,9 +1,7 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface CityRepository extends JpaRepository<City, Integer> {
 
 }
