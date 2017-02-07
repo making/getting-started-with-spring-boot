6f1c022f196e120a8a182c130cfa0ce39d8efcb8
making <makingx@gmail.com>
2017-02-07 11:01:28 +0900

    Spring 4.3 way

--- demo-web/src/main/java/com/example/CityController.java
+++ demo-web/src/main/java/com/example/CityController.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +14,12 @@ public class CityController {
 	static List<City> cities = new CopyOnWriteArrayList<>();
 	static AtomicInteger counter = new AtomicInteger(0);
 
-	@RequestMapping(value = "/cities", method = RequestMethod.GET)
+	@GetMapping("/cities")
 	List<City> getCities() {
 		return cities;
 	}
 
-	@RequestMapping(value = "/cities", method = RequestMethod.POST)
+	@PostMapping("/cities")
 	City postCities(@RequestBody City city) {
 		city.setId(counter.incrementAndGet());
 		cities.add(city);
