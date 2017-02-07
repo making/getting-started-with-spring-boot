b87be0239ea63cc9888203f81f487c415b58f959
making <makingx@gmail.com>
2017-02-07 10:43:07 +0900

    Bump to Spring Boot 1.5.1
    (cherry picked from commit f14953c67ce7266f2b4d1b12b49511252bf64587)

--- demo-web/src/test/java/com/example/DemoWebApplicationTests.java
+++ demo-web/src/test/java/com/example/DemoWebApplicationTests.java
@@ -3,11 +3,12 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoWebApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @WebAppConfiguration
 public class DemoWebApplicationTests {
 
