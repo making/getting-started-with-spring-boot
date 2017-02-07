e72741d23a54eaed2a6f94fb6cfd855043b4ec4e
making <makingx@gmail.com>
2017-02-07 10:50:25 +0900

    Polish
    (cherry picked from commit 9b092ea96dfce6ee964bf467e7d55e2de1b5b19b)

--- demo-web/src/test/java/com/example/DemoWebApplicationTests.java
+++ demo-web/src/test/java/com/example/DemoWebApplicationTests.java
@@ -2,14 +2,11 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
 public class DemoWebApplicationTests {
 
 	@Test
